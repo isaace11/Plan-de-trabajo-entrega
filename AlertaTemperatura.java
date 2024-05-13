e1d49bc739494e1dacbbfd62eda85c54c228e8fb
isaace11 <160068580+isaace11@users.noreply.github.com>
2024-05-13 16:05:18 -0500

    AlertaTemperatura.java

--- AlertaTemperatura.java
+++ AlertaTemperatura.java
@@ -1,22 +1,33 @@
 import java.util.Random;
-
-public class AlertaTemperatura {
+class AlertaTemperatura {
     private double[][] temperatureMatrix;
     private final int MATRIX_SIZE = 5;
     private final double HIGH_TEMPERATURE_THRESHOLD = 500.0;
 
-    public static void activarAlarma() {
-        System.out.println("Alarma de incendio activada");
+    public AlertaTemperatura() {
+        temperatureMatrix = new double[MATRIX_SIZE][MATRIX_SIZE];
+        initializeMatrix();
     }
 
-    public static void Incendio(String ubicacion, String gravedad) {
-        System.out.println("Incendio reportado en " + ubicacion + " con gravedad: " + gravedad);
+    private void initializeMatrix() {
+        Random random = new Random();
+        for (int i = 0; i < MATRIX_SIZE; i++) {
+            for (int j = 0; j < MATRIX_SIZE; j++) {
+                temperatureMatrix[i][j] = random.nextDouble() * 1000;
+            }
+        }
     }
 
-    public static String EstadoZona(String ubicacion) {
-        String[] estados = {"Normal", "Incendio leve", "Incendio grave"};
-        int index = (int) (Math.random() * estados.length);
-        return estados[index];
+    public void checkTemperatureAlert() {
+        for (int i = 0; i < MATRIX_SIZE; i++) {
+            for (int j = 0; j < MATRIX_SIZE; j++) {
+                if (temperatureMatrix[i][j] > HIGH_TEMPERATURE_THRESHOLD) {
+                    System.out.println("¡Alerta! Temperatura alta detectada en la posición [" + i + "][" + j + "]");
+                    return;
+                }
+            }
+        }
+        System.out.println("No se han detectado temperaturas altas en la zona.");
     }
 
     public static String evaluarTemperaturaZona(double temperatura) {
@@ -29,25 +40,5 @@ public class AlertaTemperatura {
         } else {
             return "Estado de la zona: Incendio grave";
         }
-    }
-
-    public void checkTemperatureAlert() {
-        Random random = new Random();
-        temperatureMatrix = new double[MATRIX_SIZE][MATRIX_SIZE];
-        for (int i = 0; i < MATRIX_SIZE; i++) {
-            for (int j = 0; j < MATRIX_SIZE; j++) {
-                temperatureMatrix[i][j] = random.nextDouble() * 1000;
-            }
-        }
-
-        for (int i = 0; i < MATRIX_SIZE; i++) {
-            for (int j = 0; j < MATRIX_SIZE; j++) {
-                if (temperatureMatrix[i][j] > HIGH_TEMPERATURE_THRESHOLD) {
-                    System.out.println("¡Alerta! Temperatura alta detectada en la posición [" + i + "][" + j + "]");
-                    return;
-                }
-            }
-        }
-        System.out.println("No se han detectado temperaturas altas en la zona.");
     }
 }
