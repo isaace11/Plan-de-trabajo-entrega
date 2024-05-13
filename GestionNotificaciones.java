3c1b48c049155f276b8770ef691f4507509a9a0e
isaace11 <160068580+isaace11@users.noreply.github.com>
2024-05-13 16:04:10 -0500

    GestionNotificaciones.java

--- GestionNotificaciones.java
+++ GestionNotificaciones.java
@@ -1,17 +1,26 @@
 import java.util.ArrayList;
-
-public class GestionNotificaciones {
-    public static void enviarAlertaBombero(String nombre, String direccion) {
+class GestionNotificaciones {
+    public static void enviarAlertaBombero(String direccion) {
         System.out.println("Se ha enviado una alerta a los bomberos:");
         System.out.println("¡Incendio reportado en la dirección " + direccion + "!");
     }
 
-    public static void enviarAlertaResidente(String nombre, String direccion) {
+    public static void enviarAlertaResidente(String direccion) {
         System.out.println("Se ha enviado una alerta a los residentes:");
         System.out.println("¡Incendio reportado en la dirección " + direccion + "! Por favor evacue la zona.");
     }
 
-    public static void Notificaciones(ArrayList<String> destinatarios, String mensaje) {
+    public static void reportarIncendio(String ubicacion, String gravedad) {
+        System.out.println("Incendio reportado en " + ubicacion + " con gravedad: " + gravedad);
+    }
+
+    public static String evaluarEstadoZona(String ubicacion) {
+        String[] estados = {"Normal", "Incendio leve", "Incendio grave"};
+        int index = (int) (Math.random() * estados.length);
+        return estados[index];
+    }
+
+    public static void enviarNotificaciones(ArrayList<String> destinatarios, String mensaje) {
         for (String destinatario : destinatarios) {
             System.out.println("Mensaje enviado a " + destinatario + ": " + mensaje);
         }
